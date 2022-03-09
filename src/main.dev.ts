98ebf1ee4d4debb891f354c3113d639e23bed7b9
Bảo Hưng <baohung2412@gmail.com>
2022-03-09 12:16:15 +0700

    adjust owner

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -30,8 +30,8 @@ export default class AppUpdater {
     autoUpdater.logger = log;
     autoUpdater.setFeedURL({
       provider: 'github',
-      repo: 'desktop_app',
-      owner: 'remotetoday-app',
+      repo: 'buidler-desktop',
+      owner: 'buidlerfoundation',
       private: true,
       token: '72f9f244a881d8d2d3a0c49019924b2485761d64',
     });
