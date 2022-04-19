f1b5683b28d6f4f337eabbffdaa028257d2fec48
Bảo Hưng <baohung2412@gmail.com>
2022-04-19 21:59:09 +0700

    Bump version

--- src/renderer/components/ModalTeamSetting/index.tsx
+++ src/renderer/components/ModalTeamSetting/index.tsx
@@ -103,7 +103,7 @@ const ModalTeamSetting = ({
                 </div>
               </div>
               <div className="app-version">
-                <span>1.1.58</span>
+                <span>1.1.59</span>
               </div>
             </div>
             <div className="body">
