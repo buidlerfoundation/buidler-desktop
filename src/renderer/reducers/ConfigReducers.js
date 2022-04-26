fee82d6d977b7a482c06aef837b523d641f6d049
Bảo Hưng <baohung2412@gmail.com>
2022-04-26 23:13:14 +0700

    Fix toggle otp

--- src/renderer/reducers/ConfigReducers.js
+++ src/renderer/reducers/ConfigReducers.js
@@ -14,7 +14,7 @@ const configReducers = (state = initialState, action) => {
     case actionTypes.TOGGLE_OTP: {
       return {
         ...state,
-        openOTP: payload.open != null ? payload.open : !state.openOTP,
+        openOTP: payload?.open != null ? payload?.open : !state.openOTP,
         requestOtpCode: payload?.otp || '',
       };
     }
