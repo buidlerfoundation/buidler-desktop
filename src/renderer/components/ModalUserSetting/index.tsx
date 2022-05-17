bae33d39aa7462aeb7bb3373a973105ea46dda7c
HungNguyen <baohung2412@gmail.com>
2022-05-17 22:48:04 +0700

    Bump version

--- src/renderer/components/ModalUserSetting/index.tsx
+++ src/renderer/components/ModalUserSetting/index.tsx
@@ -137,7 +137,7 @@ const ModalUserSetting = ({
             <span className="log-out-text">Logout</span>
           </div>
           <div className="app-version">
-            <span>1.1.80</span>
+            <span>1.5.0</span>
           </div>
         </div>
         <div className="body">
