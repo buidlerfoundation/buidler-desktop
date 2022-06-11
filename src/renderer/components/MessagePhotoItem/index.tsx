8b2515b3eec8eb157f6f94040f7e580556159886
HungNguyen <baohung2412@gmail.com>
2022-06-11 11:57:02 +0700

    Fix message file

--- src/renderer/components/MessagePhotoItem/index.tsx
+++ src/renderer/components/MessagePhotoItem/index.tsx
@@ -4,6 +4,56 @@ import ImgLightBox from '../ImgLightBox';
 import ImageHelper from '../../common/ImageHelper';
 import images from '../../common/images';
 
+type PhotoItemProps = {
+  photo: any;
+  handleFileClick: (file: any) => void;
+  teamId: string;
+};
+
+const PhotoItem = ({ photo, handleFileClick, teamId }: PhotoItemProps) => {
+  const handleClick = useCallback(
+    () => handleFileClick(photo),
+    [handleFileClick, photo]
+  );
+  if (photo.mimetype.includes('application')) {
+    return (
+      <div
+        className="file-item"
+        onClick={handleClick}
+        key={`${photo?.file_id}`}
+      >
+        <img alt="" src={images.icFile} />
+        <span className="file-name">{photo.original_name}</span>
+        <img alt="" src={images.icDownload} />
+      </div>
+    );
+  }
+  if (photo.mimetype.includes('video')) {
+    return (
+      <video className="photo-item video" controls key={`${photo?.file_id}`}>
+        <source
+          src={ImageHelper.normalizeImage(photo.file_url, teamId)}
+          type="video/mp4"
+        />
+      </video>
+    );
+  }
+  return (
+    <ImgLightBox
+      key={`${photo?.file_id}`}
+      originalSrc={ImageHelper.normalizeImage(photo.file_url, teamId)}
+    >
+      <img
+        className="photo-item"
+        alt=""
+        src={ImageHelper.normalizeImage(photo.file_url, teamId, {
+          h: 120,
+        })}
+      />
+    </ImgLightBox>
+  );
+};
+
 type MessagePhotoItemProps = {
   photos: Array<any>;
   teamId?: string;
@@ -15,56 +65,23 @@ const MessagePhotoItem = ({
   teamId,
   isHead,
 }: MessagePhotoItemProps) => {
-  const handleFileClick = useCallback(() => {
-    window.open(
-      ImageHelper.normalizeImage(photo.file_url, teamId, {}, true),
-      '_blank'
-    );
-  }, [teamId]);
-  const renderPhoto = useCallback(
+  const handleFileClick = useCallback(
     (photo) => {
-      if (photo.mimetype.includes('application')) {
-        return (
-          <div
-            className="file-item"
-            onClick={handleFileClick}
-            key={`${photo?.file_id}`}
-          >
-            <img alt="" src={images.icFile} />
-            <span className="file-name">{photo.original_name}</span>
-            <img alt="" src={images.icDownload} />
-          </div>
-        );
-      }
-      if (photo.mimetype.includes('video')) {
-        return (
-          <video
-            className="photo-item video"
-            controls
-            key={`${photo?.file_id}`}
-          >
-            <source
-              src={ImageHelper.normalizeImage(photo.file_url, teamId)}
-              type="video/mp4"
-            />
-          </video>
-        );
-      }
-      return (
-        <ImgLightBox
-          key={`${photo?.file_id}`}
-          originalSrc={ImageHelper.normalizeImage(photo.file_url, teamId)}
-        >
-          <img
-            className="photo-item"
-            alt=""
-            src={ImageHelper.normalizeImage(photo.file_url, teamId, {
-              h: 120,
-            })}
-          />
-        </ImgLightBox>
+      window.open(
+        ImageHelper.normalizeImage(photo.file_url, teamId, {}, true),
+        '_blank'
       );
     },
+    [teamId]
+  );
+  const renderPhoto = useCallback(
+    (photo) => (
+      <PhotoItem
+        photo={photo}
+        handleFileClick={handleFileClick}
+        teamId={teamId}
+      />
+    ),
     [handleFileClick, teamId]
   );
   if (!teamId) return null;
