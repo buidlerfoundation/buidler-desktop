0ec9fb900303656fcc1d9bc130daddd4cb767e20
HungNguyen <baohung2412@gmail.com>
2022-08-17 17:18:02 +0700

    Update UI send

--- src/renderer/pages/Home/index.tsx
+++ src/renderer/pages/Home/index.tsx
@@ -698,10 +698,10 @@ const Home = () => {
   ]);
   useEffect(() => {
     setOpenTask(false);
-    if (channelId && validateUUID(channelId)) {
+    if (channelId && validateUUID(channelId) && !!userData.user_id) {
       dispatch(getTasks(channelId));
     }
-  }, [channelId, dispatch]);
+  }, [channelId, dispatch, userData.user_id]);
   useEffect(() => {
     setOpenConversation(false);
     inputRef.current?.focus();
