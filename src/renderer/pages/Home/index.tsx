3642a8abd6a8ac1cad6dae0d0339bc0085c15a8d
HungNguyen <baohung2412@gmail.com>
2022-09-07 17:35:05 +0700

    Update previous message pin post

--- src/renderer/pages/Home/index.tsx
+++ src/renderer/pages/Home/index.tsx
@@ -899,7 +899,6 @@ const Home = () => {
               onEdit={onEditPost}
               messages={messageData[matchPostId]?.data}
               loadMoreMessage={loadMorePPMessage}
-              messageCanMore={messageData?.[matchPostId]?.canMore}
               onMoreMessage={onMorePinPostMessage}
             />
           )}
