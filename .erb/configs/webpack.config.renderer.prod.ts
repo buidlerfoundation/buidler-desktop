4001553deace48bcefa304332f70561c633b8d80
HungNguyen <baohung2412@gmail.com>
2022-11-17 17:17:40 +0700

    Update env

--- .erb/configs/webpack.config.renderer.prod.ts
+++ .erb/configs/webpack.config.renderer.prod.ts
@@ -14,6 +14,7 @@ import baseConfig from './webpack.config.base';
 import webpackPaths from './webpack.paths';
 import checkNodeEnv from '../scripts/check-node-env';
 import deleteSourceMaps from '../scripts/delete-source-maps';
+import dotenv from 'dotenv';
 
 checkNodeEnv('production');
 deleteSourceMaps();
@@ -118,6 +119,7 @@ const configuration: webpack.Configuration = {
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'production',
       DEBUG_PROD: false,
+      ...dotenv.config().parsed,
     }),
 
     new MiniCssExtractPlugin({
