b0c702c29b05222afb96d3c4cd0d8e13f163b122
HungNguyen <baohung2412@gmail.com>
2022-11-17 17:01:26 +0700

    Update env

--- .erb/configs/webpack.config.main.prod.ts
+++ .erb/configs/webpack.config.main.prod.ts
@@ -11,6 +11,7 @@ import baseConfig from './webpack.config.base';
 import webpackPaths from './webpack.paths';
 import checkNodeEnv from '../scripts/check-node-env';
 import deleteSourceMaps from '../scripts/delete-source-maps';
+import dotenv from 'dotenv';
 
 checkNodeEnv('production');
 deleteSourceMaps();
@@ -65,6 +66,7 @@ const configuration: webpack.Configuration = {
       NODE_ENV: 'production',
       DEBUG_PROD: false,
       START_MINIMIZED: false,
+      ...dotenv.config().parsed,
     }),
   ],
 
